1490acbd0c7fe77c7a9618362a1019fda372b2c1
V1rucov <rujnikov2015@outlook.com>
2025-07-23 15:26:20 +0300

    косяк на боте

---
+++ run_bot.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+export BOT_TOKEN="8032192542:AAE-mmWaOBBVtbubkZfLOWICvqmubkaoKxg"
+export ADMIN_ID="1496419877"
+python3 run_bot.py
--- run_web.sh
+++ run_web.sh
@@ -1,4 +1,2 @@
 #!/bin/bash
-export BOT_TOKEN="8032192542:AAE-mmWaOBBVtbubkZfLOWICvqmubkaoKxg"
-export ADMIN_ID="1496419877"
 gunicorn -w 4 -b 0.0.0.0:5000 wsgi:app
